407939153331a94cd57dc6e6f88892c7abbb37db
wangzeqi <wangzeqi@xiaomi.com>
2021-08-16 17:40:06 +0800

    自定义indexView

--- kjIndexView/src/main/java/com/kbjay/fragment/kjindexview/KJIndexView.kt
+++ kjIndexView/src/main/java/com/kbjay/fragment/kjindexview/KJIndexView.kt
@@ -4,51 +4,91 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 
-class KJIndexView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+class KJIndexView(context: Context, attributeSet: AttributeSet) :
+    LinearLayout(context, attributeSet) {
 
-    private var unSelectedColor: Int
-    private var selectedColor: Int
-
-    private var itemWidth: Int
-    private var itemHeight: Int
-
-    private var paint1: Paint
-    private var paint2: Paint
-
-    var itemCount = 10
+    private var currentTotalSize = 0
+    private var preSelectedItemIndex = 0
+    private var unSelectedDrawable: Drawable?
+    private var selectedDrawable: Drawable?
+    private var cap = 0
 
     init {
-        itemHeight = 40
-        itemWidth = 100
-        unSelectedColor = Color.DKGRAY
-        selectedColor = Color.GRAY
-        paint1 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-            color = unSelectedColor
+        val obtainStyledAttributes =
+            context.obtainStyledAttributes(attributeSet, R.styleable.KJIndexView)
+        selectedDrawable =
+            obtainStyledAttributes.getDrawable(R.styleable.KJIndexView_selected_drawable)
+                ?: context.getDrawable(R.mipmap.index_selected)
+        unSelectedDrawable =
+            obtainStyledAttributes.getDrawable(R.styleable.KJIndexView_unSelected_drawable)
+                ?: context.getDrawable(R.mipmap.index_unselected)
+        cap = obtainStyledAttributes.getDimensionPixelSize(R.styleable.KJIndexView_item_cap,0)
+        obtainStyledAttributes.recycle()
+
+        orientation = HORIZONTAL
+        layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    fun update(total: Int, currentIndex: Int) {
+        if (total <= 0 || currentIndex >= total) {
+            return
         }
-        paint2 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-            color = selectedColor
+
+        if (currentTotalSize == total) {
+            (getChildAt(preSelectedItemIndex) as ImageView).setImageDrawable(unSelectedDrawable)
+            (getChildAt(currentIndex) as ImageView).setImageDrawable(selectedDrawable)
+            preSelectedItemIndex = currentIndex
+        } else {
+            removeAllViews()
+            for (i in 0 until total) {
+                if (currentIndex == i) {
+                    addView(createSelectedView(i == total - 1))
+                } else {
+                    addView(createUnSelectedView(i == total - 1))
+                }
+            }
+            currentTotalSize = total
+            preSelectedItemIndex = currentIndex
+        }
+
+        postInvalidate()
+    }
+
+    private fun createUnSelectedView(isLastItem: Boolean): ImageView {
+        return ImageView(context).apply {
+            setImageDrawable(unSelectedDrawable)
+            val marginLayoutParams = MarginLayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            if (!isLastItem) {
+                marginLayoutParams.marginEnd = cap
+            }
+            layoutParams = marginLayoutParams
         }
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        canvas?.drawRect(
-            (measuredWidth / 2 - itemWidth / 2).toFloat(),
-            (measuredHeight / 2 - itemHeight / 2).toFloat(),
-            (measuredWidth / 2 + itemWidth / 2).toFloat(),
-            (measuredHeight / 2 + itemHeight / 2).toFloat(),
-            paint2
-        )
-        canvas?.skew(1f, 0f)
-        canvas?.drawRect(
-            (measuredWidth / 2 - itemWidth / 2).toFloat(),
-            (measuredHeight / 2 - itemHeight / 2).toFloat(),
-            (measuredWidth / 2 + itemWidth / 2).toFloat(),
-            (measuredHeight / 2 + itemHeight / 2).toFloat(),
-            paint1
-        )
+    private fun createSelectedView(isLastItem: Boolean): ImageView {
+        return ImageView(context).apply {
+            setImageDrawable(selectedDrawable)
+            val marginLayoutParams = MarginLayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            if (!isLastItem) {
+                marginLayoutParams.marginEnd = cap
+            }
+            layoutParams = marginLayoutParams
+        }
     }
 }
--- kjIndexView/src/main/java/com/kbjay/fragment/kjindexview/MainActivity.kt
+++ kjIndexView/src/main/java/com/kbjay/fragment/kjindexview/MainActivity.kt
@@ -2,10 +2,25 @@ package com.kbjay.fragment.kjindexview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var kjIndexView: KJIndexView
+
+    private var total = 10
+    private var current = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        kjIndexView = findViewById(R.id.index)
+        kjIndexView.update(total, current)
+    }
+
+    fun update(view: View) {
+        current++
+        if (current == total) {
+            current = 0
+        }
+        kjIndexView.update(total, current)
     }
 }
